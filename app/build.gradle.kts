59930e45c7a909548fa05688f87427694a715384
Cesar Valiente Gordo <cevalien@microsoft.com>
2024-12-13 12:42:20 +0100

    We define now its own namespace and applicationId for hipsterapp too

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,3 +2,10 @@ plugins {
     id("conventionPluginsApp.android.application")
     id("conventionPluginsApp.android.app.compose")
 }
+
+android {
+    namespace = "com.cesarvaliente.hipsterapp"
+    defaultConfig {
+        applicationId = "com.cesarvaliente.hipsterapp"
+    }
+}
