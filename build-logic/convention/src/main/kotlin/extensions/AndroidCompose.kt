595eeda8f565316c88427bbd360ec610ae41f082
Cesar Valiente Gordo <cevalien@microsoft.com>
2024-12-13 13:01:22 +0100

    Making the code a bit more elegant

--- build-logic/convention/src/main/kotlin/extensions/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/extensions/AndroidCompose.kt
@@ -13,22 +13,24 @@ fun Project.configureAndroidCompose(
             versionCatalog().findVersion("androidx-compose-compiler").get().toString()
 
         dependencies {
-            add("implementation", versionCatalog().findLibrary("activity.compose").get())
-            add("implementation", platform(versionCatalog().findLibrary("compose.bom").get()))
-            add("implementation", versionCatalog().findLibrary("compose.ui").get())
-            add("implementation", versionCatalog().findLibrary("compose.graphics").get())
-            add("implementation", versionCatalog().findLibrary("compose.preview").get())
-            add("implementation", versionCatalog().findLibrary("compose.material3").get())
-            add("implementation", versionCatalog().findLibrary("compose.icons").get())
+            with(versionCatalog()) {
+                add("implementation", findLibrary("activity.compose").get())
+                add("implementation", platform(findLibrary("compose.bom").get()))
+                add("implementation", findLibrary("compose.ui").get())
+                add("implementation", findLibrary("compose.graphics").get())
+                add("implementation", findLibrary("compose.preview").get())
+                add("implementation", findLibrary("compose.material3").get())
+                add("implementation", findLibrary("compose.icons").get())
 
-            //testing
-            add("testImplementation", versionCatalog().findLibrary("junit").get())
-            add("androidTestImplementation", versionCatalog().findLibrary("ext.junit").get())
-            add("androidTestImplementation", versionCatalog().findLibrary("espresso.core").get())
-            add("androidTestImplementation", platform(versionCatalog().findLibrary("compose.bom").get()))
-            add("androidTestImplementation", versionCatalog().findLibrary("ui.test.junit4").get())
-            add("debugImplementation", versionCatalog().findLibrary("ui.tooling").get())
-            add("debugImplementation", versionCatalog().findLibrary("ui.test.manifest").get())
+                //testing
+                add("testImplementation", findLibrary("junit").get())
+                add("androidTestImplementation", findLibrary("ext.junit").get())
+                add("androidTestImplementation", findLibrary("espresso.core").get())
+                add("androidTestImplementation", platform(findLibrary("compose.bom").get()))
+                add("androidTestImplementation", findLibrary("ui.test.junit4").get())
+                add("debugImplementation", findLibrary("ui.tooling").get())
+                add("debugImplementation", findLibrary("ui.test.manifest").get())
+            }
         }
     }
 }
